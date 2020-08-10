e171187784ca24c33f7c3e8e8ccf5f12e2cae8fb
cochoa_hiberus <cochoa@hiberus.com>
2020-08-10 08:35:29 +0200

    realiza ping durante el tiempo que la regla creada por el flowmod está vigente. Luego se interrumpe la comunicación

--- README.md
+++ README.md
@@ -2,3 +2,31 @@
 Implementar una aplicación SDN completa, específicamente un balanceador de carga en POX
 
 
+    Handle packet in messages from the switch to implement above algorithm.
+    
+
+    To behave as a transparent proxy between the clients and the servers
+    the load balancer has to perform the following tasks:
+
+    1. Reply client ARP requests with the fake MAC from the switch/frontend/load-balancer when clients 
+        search for the MAC of the switch/frontend IP. Please save the information of the ARP request; namely,
+        source MAC of the client and switch input port of the ARP request
+        packet, so that when the load balancer has to direct flows to the
+        clients, it will already know the address and the output port to forward the packages.
+    
+    2. Reply pool servers ARP request to client IPs with messaged ARP responses. The responses will contain 
+        the fake MAC address associated with the load balancer. Note that by now, the load balancer
+        should already know the real MAC address of the client.
+
+    3. The load balancer should forward flows from the clients towards
+        the servers using the following balancing approaches: random and
+        round-robin. All packages received by the pool servers will have
+        the fake MAC address of the load balancer and the IP of the client
+        unmodified.
+
+    4. Forward the packages from the pool servers to the clients after updating the source IP and MAC 
+        addresses from that of the pool servers
+        to the addresses of the load balancer. Clients should never know
+        about the existence of the pool servers and cannot see any package
+        from them. In other words, the load balancer will be completely
+        transparent to them.
--- load_balancer.py
+++ load_balancer.py
@@ -26,15 +26,13 @@ class LoadBalancerSwitch (object):
     # Switch we'll be adding L2 learning switch capabilities to
     self.connection = connection
 
-    self.mac = EthAddr("00:00:00:00:00:" + str(HOST_NUMBER+1))
-    self.ip = IPAddr("10.0.0." + str(HOST_NUMBER+1))
+    self.switchMac = EthAddr("00:00:00:00:00:" + str(HOST_NUMBER+1))
+    self.switchIp = IPAddr("10.0.0." + str(HOST_NUMBER+1))
     # self.clientsMacs = [EthAddr("00:00:00:00:00:" + str(x)) for x in  range(1,HOST_NUMBER/2 +1) ]
     # self.serversMacs = [EthAddr("00:00:00:00:00:" + str(x)) for x in  range(HOST_NUMBER/2 +1,HOST_NUMBER +1) ]
-    self.clientMacs = {}
-    self.serverMacs = {}
+    self.hosts = {}
     self.clientsIps = [IPAddr("10.0.0." + str(x)) for x in  range(1,HOST_NUMBER/2 +1) ]
     self.serversIps = [IPAddr("10.0.0." + str(x)) for x in  range(HOST_NUMBER/2 +1,HOST_NUMBER +1) ]
-    self.ports = {}
     # We want to hear PacketIn messages, so we listen
     # to the connection
     connection.addListeners(self)
@@ -49,37 +47,7 @@ class LoadBalancerSwitch (object):
     log.debug(self.serversIps)
 
   def _handle_PacketIn (self, event):
-    """
-    Handle packet in messages from the switch to implement above algorithm.
-    
 
-    To behave as a transparent proxy between the clients and the servers
-    the load balancer has to perform the following tasks:
-
-    1. Reply client ARP requests with the fake MAC from the switch/frontend/load-balancer when clients 
-        search for the MAC of the switch/frontend IP. Please save the information of the ARP request; namely,
-        source MAC of the client and switch input port of the ARP request
-        packet, so that when the load balancer has to direct flows to the
-        clients, it will already know the address and the output port to forward the packages.
-    
-    2. Reply pool servers ARP request to client IPs with messaged ARP responses. The responses will contain 
-        the fake MAC address associated with the load balancer. Note that by now, the load balancer
-        should already know the real MAC address of the client.
-
-    3. The load balancer should forward flows from the clients towards
-        the servers using the following balancing approaches: random and
-        round-robin. All packages received by the pool servers will have
-        the fake MAC address of the load balancer and the IP of the client
-        unmodified.
-
-    4. Forward the packages from the pool servers to the clients after updating the source IP and MAC 
-        addresses from that of the pool servers
-        to the addresses of the load balancer. Clients should never know
-        about the existence of the pool servers and cannot see any package
-        from them. In other words, the load balancer will be completely
-        transparent to them.
-
-    """
 
     log.info("PACKET_IN")
     inport = event.port
@@ -89,11 +57,6 @@ class LoadBalancerSwitch (object):
       return 0
       # return random.randint(0,5)
 
-    def find_client_mac_by_ip(ip):
-      return self.clientsMacs[self.clientsIps.index(ip)]
-
-    def find_server_mac_by_ip(ip):
-      return self.serversMacs[self.serversIps.index(ip)]
     """
     if arp request
         we dont know the destination
@@ -109,117 +72,113 @@ class LoadBalancerSwitch (object):
         if packet.payload.opcode == arp.REQUEST:
             log.info("arp.REQUEST == true")
             a = packet.next
-            self.ports[a.hwsrc] = inport
-            log.info("self.ports")
-            log.info(self.ports)
-            if a.hwsrc in self.clientsMacs:
-              selectedServer = select_server()
-              a.hwsrc = self.mac
-              a.hwdst = self.serversMacs[selectedServer]
-              a.protodst = self.serversIps[selectedServer]
-              e = ethernet(type=ethernet.ARP_TYPE, src=self.mac, dst=ETHER_BROADCAST)
+            self.hosts[a.protosrc] = (a.hwsrc, inport)
+            log.info("self.hosts")
+            log.info(self.hosts)
+            if a.protosrc in self.clientsIps:
+              selectedServer = self.serversIps[select_server()]
+              a.hwsrc = self.switchMac
+              a.protodst = selectedServer
+              e = ethernet(type=ethernet.ARP_TYPE, src=self.switchMac, dst=ETHER_BROADCAST)
               e.set_payload(a)
               log.debug("%s requesting ARP to %s" % (a.protosrc, a.protodst))
               msg = of.ofp_packet_out()
               msg.data = e.pack()
               msg.actions.append(of.ofp_action_output(port = of.OFPP_FLOOD))
+              event.connection.send(msg)
+              return
+            elif a.protosrc in self.serversIps:
+              log.warning("Receiving an ARP request from a server!!")
+              log.debug("%s (%s) => %s (%s)" % (a.protosrc, a.hwsrc, a.protodst, a.hwdst))
+              self.hosts[a.protosrc] = (a.hwsrc, inport)
+              r = arp()
+              r.opcode = arp.REPLY
+              r.hwsrc = self.switchMac
+              r.hwdst = a.hwsrc
+              r.protosrc = a.protodst
+              r.protodst = a.protosrc
+              e = ethernet(type=ethernet.ARP_TYPE, src=self.switchMac, dst=a.hwsrc)
+              e.set_payload(r)
+              log.debug("%s (%s) replying ARP to %s (%s)" % (r.protosrc, r.hwsrc, r.protodst, r.hwdst))
+              msg = of.ofp_packet_out()
+              msg.data = e.pack()
+              msg.actions.append(of.ofp_action_output(port = inport))
               msg.in_port = inport
               event.connection.send(msg)
               return
-            elif a.hwsrc in self.serversMacs:
-              log.debug("WARNING: Receiving an ARP request from a server!!")
-              # a.hwdst = a.hwsrc
-              # a.hwsrc = self.mac
-              # a.opcode = arp.REPLY
-              # a.protodst = a.protosrc
-              # a.protosrc = self.ip
-              # e = ethernet(type=ethernet.ARP_TYPE, src=self.mac, dst=a.hwdst)
-              # e.set_payload(a)
-              # log.debug("%s replying ARP to %s" % (self.mac, a.protodst))
-              # msg = of.ofp_packet_out()
-              # msg.data = e.pack()
-              # msg.actions.append(of.ofp_action_output(port = inport))
-              # msg.in_port = inport
-              # event.connection.send(msg)
-              return
 
-            #send this packet to the switch
-            #see section below on this topic
         elif packet.payload.opcode == arp.REPLY:
             log.info("arp.REPLY == true")
             a = packet.next
-            self.ports[a.hwsrc] = inport
-            log.debug("%s replying ARP to %s" % (a.protosrc, a.protodst))
-            if a.hwsrc in self.serversMacs:
-              msg = of.ofp_flow_mod() 
-              msg.match.dl_src = find_client_mac_by_ip(a.protodst)
-              msg.match.dl_dst = self.mac
-              msg.match.nw_src = a.protodst
-              msg.match.nw_dst = self.ip
+            self.hosts[a.protosrc] = (a.hwsrc, inport)
+            log.info("self.hosts")
+            log.info(self.hosts)
+            log.debug("%s (%s) replying ARP to %s (%s)" % (a.protosrc, a.hwsrc, a.protodst, a.hwdst))
+            if a.protosrc in self.serversIps:
+              clientIp = a.protodst
+              serverIp = a.protosrc
+              clientMac, clientPort = self.hosts[clientIp]
+              serverMac, serverPort = self.hosts[serverIp]
+
+              msg = of.ofp_flow_mod()
+              msg.match.dl_type = 0x800 
+              msg.match.dl_src = clientMac
+              msg.match.dl_dst = self.switchMac
+              msg.match.nw_src = clientIp
+              msg.match.nw_dst = self.switchIp
               msg.idle_timeout = 10
               msg.hard_timeout = 10
               actions = []
-              actions.append(of.ofp_action_dl_addr.set_src(self.mac))
-              actions.append(of.ofp_action_dl_addr.set_dst(find_server_mac_by_ip(a.protosrc)))
-              actions.append(of.ofp_action_nw_addr.set_src(a.protodst))
-              actions.append(of.ofp_action_nw_addr.set_dst(a.protosrc))
-              actions.append(of.ofp_action_output(port = self.ports.get(find_server_mac_by_ip(a.protosrc))))
+              actions.append(of.ofp_action_dl_addr.set_src(self.switchMac))
+              actions.append(of.ofp_action_dl_addr.set_dst(serverMac))
+              actions.append(of.ofp_action_nw_addr.set_src(clientIp))
+              actions.append(of.ofp_action_nw_addr.set_dst(serverIp))
+              actions.append(of.ofp_action_output(port = serverPort))
               msg.actions = actions
               event.connection.send(msg)
-
+              log.debug("First flowMod: from %s (%s) ==> %s (%s)" % (clientIp,clientMac,serverIp,serverMac))
 
               msg = of.ofp_flow_mod() 
-              msg.match.dl_src = find_server_mac_by_ip(a.protosrc)
-              msg.match.dl_dst = self.mac
-              msg.match.nw_src = a.protosrc
-              msg.match.nw_dst = a.protodst
+              msg.match.dl_type = 0x800 
+              msg.match.dl_src = serverMac
+              msg.match.dl_dst = self.switchMac
+              msg.match.nw_src = serverIp
+              msg.match.nw_dst = clientIp
               msg.idle_timeout = 10
               msg.hard_timeout = 10
               actions = []
-              actions.append(of.ofp_action_dl_addr.set_src(self.mac))
-              actions.append(of.ofp_action_dl_addr.set_dst(find_client_mac_by_ip(a.protodst)))
-              actions.append(of.ofp_action_nw_addr.set_src(self.ip))
-              actions.append(of.ofp_action_nw_addr.set_dst(a.protodst))
-              actions.append(of.ofp_action_output(port = self.ports.get(find_client_mac_by_ip(a.protodst))))
+              actions.append(of.ofp_action_dl_addr.set_src(self.switchMac))
+              actions.append(of.ofp_action_dl_addr.set_dst(clientMac))
+              actions.append(of.ofp_action_nw_addr.set_src(self.switchIp))
+              actions.append(of.ofp_action_nw_addr.set_dst(clientIp))
+              actions.append(of.ofp_action_output(port = clientPort))
               msg.actions = actions
-              a.hwdst = find_client_mac_by_ip(a.protodst)
-              a.hwsrc = EthAddr(self.mac)
-              a.protosrc = self.ip
-              e = ethernet(type=packet.type, src=self.mac, dst=find_client_mac_by_ip(a.protodst))
-              e.set_payload(a)
-              # msg.data = e.pack()
               event.connection.send(msg)
+              log.debug("Second flowMod: from %s (%s) <== %s (%s)" % (clientIp,clientMac,serverIp,serverMac))
 
 
-
-
-              a.hwdst = find_client_mac_by_ip(a.protodst)
-              a.hwsrc = self.mac
-              a.protosrc = self.ip
-              e = ethernet(type=packet.type, src=self.mac, dst=find_client_mac_by_ip(a.protodst))
-              e.set_payload(a)
-
+              r = arp()
+              r.opcode = arp.REPLY
+              r.hwsrc = self.switchMac
+              r.hwdst = clientMac
+              r.protosrc = self.switchIp
+              r.protodst = clientIp
+              e = ethernet(type=ethernet.ARP_TYPE, src=self.switchMac, dst=clientMac)
+              e.set_payload(r)
+              log.debug("%s Replying ARP to %s" % (r.protosrc, r.protodst))
               msg = of.ofp_packet_out()
               msg.data = e.pack()
-              msg.actions.append(of.ofp_action_output(port = self.ports.get(find_client_mac_by_ip(a.protodst))))
+              msg.actions.append(of.ofp_action_output(port = clientPort))
               msg.in_port = inport
               event.connection.send(msg)
+              log.debug("Third packetOut: from %s (%s) -> %s (%s)" % (r.protosrc,r.hwsrc,r.protodst,r.hwdst))
 
-
-
-
-
-            """
-
-
-            
-            """
-            # log.info("packet.payload.opcode == arp.REPLY: Probably do something smart here")
             return
         else:
             log.info("Some other ARP opcode, probably do something smart here")
     elif packet.type == ethernet.IP_TYPE:
       log.info("Some IP_TYPE opcode, probably do something smart here")
+      
 
 
 class load_balancer (object):
